44ef3125954a67b4538cdcacb50464ec6b063855
1048938 <1048938@486E303552.lwsd.k12wa.org>
2024-09-17 11:34:30 -0700

    projects for unit1

---
+++ src/helloWorld/First_Pikachu.java
@@ -0,0 +1,10 @@
+package helloWorld;
+
+public class First_Pikachu {
+	public static void main(String[] args) {
+		System.out.println("(\\__/)");
+		System.out.println("(o^.^)");
+		System.out.println("(_(\")(\")");
+
+	}
+}
---
+++ src/helloWorld/Reading.java
@@ -0,0 +1,11 @@
+package helloWorld;
+
+public class Reading {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("The Time of the Butterflies");
+
+	}
+
+}
